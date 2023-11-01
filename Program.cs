96cef4db31be21c3d34a09223ba9ad946340f5bf
deyner2002 <solano27deiner@gmail.com>
2023-11-01 16:54:33 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using App.ScopedService;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<ScopedBackgroundService>();
+        services.AddScoped<IScopedProcessingService, DefaultScopedProcessingService>();
+        var kafkaConfig = new ConsumerConfig();
+        var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .Build();
+        kafkaConfig.BootstrapServers = configuration.GetValue<string>("Kafka:BootstrapServers");
+        kafkaConfig.AutoOffsetReset = AutoOffsetReset.Earliest;
+        kafkaConfig.GroupId = configuration.GetValue<string>("Kafka:GroupId");
+
+        kafkaConfig.BootstrapServers = "localhost:9092";
+        services.AddSingleton<IConsumer<string, string>>(new ConsumerBuilder<string, string>(kafkaConfig).Build());
+    });
+
+IHost host = builder.Build();
+host.Run();
---
+++ Task/ScopedBackgroundService.cs
@@ -0,0 +1,41 @@
+﻿namespace App.ScopedService;
+using Confluent.Kafka;
+using static Confluent.Kafka.ConfigPropertyNames;
+
+public sealed class DefaultScopedProcessingService : IScopedProcessingService
+{
+    private int _executionCount;
+    private readonly ILogger<DefaultScopedProcessingService> _logger;
+    private readonly IConsumer<string, string> _kafkaConsumer;
+
+    public DefaultScopedProcessingService(
+        ILogger<DefaultScopedProcessingService> logger, IConsumer<string, string> kafkaConsumer)
+    {
+        _logger = logger;
+        _kafkaConsumer = kafkaConsumer;
+        _kafkaConsumer.Subscribe("test-kafka");
+    }
+
+    public async Task DoWorkAsync(CancellationToken stoppingToken)
+    {
+        CancellationTokenSource token = new();
+        bool bandera = true;
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            ++_executionCount;
+
+            _logger.LogInformation(
+                "{ServiceName} working, execution count: {Count}",
+                nameof(DefaultScopedProcessingService),
+                _executionCount);
+
+            var response = _kafkaConsumer.Consume(token.Token);
+            if (response.Message != null)
+            {
+                var message = response.Message.Value;
+
+                // meter logica 
+            }
+        }
+    }
+}
